99074a761502c1e113bd984f7aa6540cb61d4906
bernard <bernard@pinochet.fr>
2024-08-24 21:11:13 +0200

    Suppression fichiers copies vers le nouveau repo Jarduino

--- jarduino2/jard_rs485_master/jard_rs485_master/spec.d
+++
@@ -1,2 +0,0 @@
-spec.o: \
- C:/Users/Utilisateur/jarduino/.metadata/.plugins/org.eclipse.cdt.managedbuilder.core/spec.cpp
--- jarduino2/jard_rs485_master/jard_rs485_wifi_test/wifi_code.h
+++
@@ -1,4 +0,0 @@
-#define STASSID "Maison"
-#define STAPSK  "C6EA0D7BBDB1D8E4B3F7F2A26D"
-
-#define OTA_NAME "Petuino"
